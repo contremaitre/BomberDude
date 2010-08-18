9479d6316c66bac1d74def2ebc7b254086f2ddaa
Sébastien Escudier <sebastien-devel@celeos.eu>
2010-08-18 13:20:28 +0200

    License under gpl v3

--- GameField.cpp
+++ GameField.cpp
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2010 Sébastien Escudier
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "GameField.h"
 #include <QKeyEvent>
 
@@ -61,7 +78,7 @@ bool GameField::move(int id, int direction)
 
 void GameField::pixmapChanged(int pos)
 {
-    qDebug("GameField pixmapChanged %d",pos);
+    //qDebug("GameField pixmapChanged %d",pos);
     //QGraphicsCaseItem *m_case = caseList.getCase(pos);
     //scene->addItem(m_case->getItem());
 }
