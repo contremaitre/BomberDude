3c4c4cf0570c76e208217f1e86d8eade76877371
Sébastien Escudier <sebastien-devel@celeos.eu>
2010-11-24 14:24:13 +0100

    use constant.h values for some players caracteristics

--- common/constant.h
+++ common/constant.h
@@ -27,9 +27,10 @@
 #define MAP_SIZE 20
 
 //Game
-#define MAX_NB_PLAYER 4
-#define DEFAULT_BOMB_DURATION 4000 //ms
-#define DEFAULT_BOMB_RANGE 1
+#define MAX_NB_PLAYER 4 //for random maps
+#define DEFAULT_BOMB_DURATION 100 //ticks
+#define DEFAULT_BOMB_RANGE 4
+#define DEFAULT_BOMB_CAPACITY 3
 //how fast the bomb is blinking (ms)
 #define BOMB_BLINK_INTERVAL 500
 
