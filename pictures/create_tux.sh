7bc18355b26947a7a4b23679f084f044959f2286
Sébastien Escudier <sebastien-devel@celeos.eu>
2011-08-25 18:48:50 +0200

    improve tux.svg and the script

--- pictures/create_tux.sh
+++ pictures/create_tux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo usage : $0 origin_name dest_name orig_color dest_color
-echo example : $0 tux_down  tux2_down 000000 ffffff will create tux_down\{0,n\}.svg \-\> tux2_down\{0,n\}.png
+echo usage : $0 orig dest orig_color dest_color
+echo example : $0 tux tux 000000 ffffff will convert tux\{0,n\}.svg to tux\{0,n\}_\{down,up,left,right\}.png
 width=50
 i=0
 while true
@@ -10,11 +10,14 @@ do
   if [ ! -e $in ]; then
     break
   fi
-  echo $in > $out
   sed s/$3/$4/ $in > tmp.svg
-  inkscape -h $width tmp.svg -e $out
-  rm tmp.svg
+  inkscape -h $width tmp.svg -e tmp.png
+  cp tmp.png $2$i"_down.png"
+  convert -rotate 90 tmp.png $2$i"_right".png
+  convert -rotate 180 tmp.png $2$i"_up".png
+  convert -rotate 270 tmp.png $2$i"_left".png
   echo ok
+  rm tmp.png tmp.svg
   ((i++))
 done
 
