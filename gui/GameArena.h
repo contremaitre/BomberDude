f5e8877c337c7e0e1b86cc21feb1cdd10d3edb8c
Sébastien Escudier <sebastien-devel@celeos.eu>
2011-12-16 20:31:55 +0100

    - Do not emit playerMoved at each heartbeat, but only when the player actualy tried to move - send the current direction of the player to the clients

--- gui/GameArena.h
+++ gui/GameArena.h
@@ -86,7 +86,7 @@ public:
     void clear();
 
 private slots:
-    void movePlayer(int player, int x, int y);
+    void movePlayer(int player, int x, int y, int heading);
     void slotPlayerSickChanged(int player, bool sick);
     void blockChanged(int);
 	void blockChanged(int x, int y);
