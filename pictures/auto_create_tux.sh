7434a06f9e62de045aa43e29848efe60813832d2
lartick <lartick@gmail.com>
2012-04-09 15:24:36 +0200

    change tux pictures

---
+++ pictures/auto_create_tux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./create_tux.sh tux tux0 00aad4 ffffff
+./create_tux.sh tux tux1 00aad4 000000
+./create_tux.sh tux tux2 00aad4 ff0000
+./create_tux.sh tux tux3 00aad4 00ff00
+./create_tux.sh tux tux4 00aad4 0000ff
+./create_tux.sh tux tux5 00aad4 ffff00
